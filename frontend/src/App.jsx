590c7387bc283f5c27cceb2dac89efb2e5582147
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-08 18:20:52 -0300

    Add movie delete functionality

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { AddMovie } from "./AddMovie"
+import { MovieLibrary } from "./MovieLibrary"
+function App() {
+  return (
+    <>
+
+      <h1>Movie Library</h1>
+      <MovieLibrary />
+      <AddMovie />
+    </>
+  )
+}
+
+export default App
